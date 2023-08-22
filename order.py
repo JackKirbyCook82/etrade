132c5aa540733ccde318bb5f12f722b1094b634a
JackKirbyCook82 <jackcookchee@yahoo.com>
2023-08-21 22:29:02 -0500

    Updated 8/17/2023

--- order.py
+++ order.py
@@ -16,6 +16,7 @@ from webscraping.webpayloads import WebPayload
 from webscraping.webpages import WebJsonPage
 from support.pipelines import Uploader
 from support.dispatchers import kwargsdispatcher
+from finance.securities import Securities, Positions
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
@@ -25,12 +26,12 @@ __license__ = ""
 
 
 Status = enum.StrEnum("Status", [("PLACED", "OPEN"), ("EXECUTED", "EXECUTED"), ("REVOKED", "CANCEL_REQUESTED"), ("CANCELLED", "CANCELLED"), ("EXPIRED", "EXPIRED"), ("REJECTED", "REJECTED")])
-Contents = enum.StrEnum("ContentType", [("STOCKS", "EQ"), ("OPTIONS", "OPTN"), ("SPREADS", "SPREADS")])
+OrderType = enum.StrEnum("OrderType", [("STOCKS", "EQ"), ("OPTIONS", "OPTN"), ("SPREADS", "SPREADS")])
 Pricing = enum.StrEnum("PriceType", [("MARKET", "MARKET"), ("LIMIT", "LIMIT"), ("STOP", "STOP"), ("STOPLIMIT", "STOP_LIMIT"), ("DEBIT", "NET_DEBIT"), ("CREDIT", "NET_CREDIT")])
-Tenure = enum.StrEnum("OrderType", [("DAY", "GOOD_FOR_DAY"), ("DATE", "GOOD_TILL_DATE"), ("IMMEDIATE", "IMMEDIATE_OR_CANCEL"), ("FILLKILL", "FILL_OR_KILL")])
+Tenure = enum.StrEnum("Tenure", [("DAY", "GOOD_FOR_DAY"), ("DATE", "GOOD_TILL_DATE"), ("IMMEDIATE", "IMMEDIATE_OR_CANCEL"), ("FILLKILL", "FILL_OR_KILL")])
 Session = enum.StrEnum("Session", [("MARKET", "REGULAR"), ("EXTENDED", "EXTENDED")])
 Concurrent = enum.StrEnum("Concurrent", [("TRUE", "true"), ("FALSE", "false")])
-Action = enum.StrEnum("Action", [("LONG", "BUY"), ("SHORT", "SELL")])
+Action = enum.StrEnum("Action", [("BUY", "BUY"), ("BUY", "SELL")])
 Basis = enum.StrEnum("QuantityType", [("QUANTITY", "QUANTITY"), ("CURRENCY", "DOLLAR")])
 SecurityType = enum.StrEnum("SecurityType", [("STOCK", "STOCK"), ("OPTION", "OPTION")])
 OptionType = enum.StrEnum("OptionType", [("PUT", "PUT"), ("CALL", "CALL")])
@@ -49,34 +50,50 @@ class ETradeOrdersURL(WebURL):
     def path_cancel(cls, *args, account, **kwargs): return "/v1/accounts/{account}/orders/cancel.json".format(account=str(account))
 
 
-class ETradeOrderPayload(WebPayload, locator="//Order", key="orders", collection=True):
-    class Price(WebPayload, locator="//priceValue", key="price"): pass
-    class Pricing(WebPayload, locator="//priceType", key="terms"): pass
-    class Contents(WebPayload, locator="//orderType", key="contents"): pass
-    class Tenure(WebPayload, locator="//orderTerm", key="tenure"): pass
-    class Session(WebPayload, locator="//marketSession", key="session"): pass
-    class Concurrent(WebPayload, locator="//allOrNone", key="concurrent"): pass
+stock_contents = dict(ticker="//symbol", securitytype="//securityType")
+option_contents = dict(optiontype="//optionType", strike="//strikePrice", year="//expiryYear", month="//expiryMonth", day="//expiryDay")
+instrument_contents = dict(action="//orderAction", basis="//quantityType", quantity="//quantity")
+order_contents = dict(price="//priceValue", pricing="//priceType", ordertype="//orderType", tenure="//orderTerm", session="//marketSession", concurrent="//allOrNone")
+place_contents = dict(previews="//PreviewIds/previewId")
 
-    class Instruments(WebPayload, locator="//Instrument", key="instruments", collection=True):
-        class Action(WebPayload, locator="//orderAction", key="action"): pass
-        class Basis(WebPayload, locator="//quantityType", key="basis"): pass
-        class Quantity(WebPayload, locator="//quantity", key="quantity"): pass
+class Product(WebPayload, locator="//Product", key="product"): pass
+class StockProduct(Product, contents=stock_contents, securitytype=SecurityType.STOCK): pass
+class OptionProduct(Product, contents=option_contents, securitytype=SecurityType.OPTION): pass
+class PutProduct(OptionProduct, optiontype=OptionType.PUT): pass
+class CallProduct(OptionProduct, optiontype=OptionType.CALL): pass
 
-        class Product(WebPayload, locator="//Product", key="product"):
-            class Ticker(WebPayload, locator="//symbol", key="ticker"): pass
-            class SecurityType(WebPayload, locator="//securityType", key="type"): pass
-            class OptionType(WebPayload, locator="//callPut", key="option", optional=True): pass
-            class Strike(WebPayload, locator="//strikePrice", key="strike", optional=True): pass
-            class Year(WebPayload, locator="//expiryYear", key="year", optional=True): pass
-            class Month(WebPayload, locator="//expiryMonth", key="month", optional=True): pass
-            class Day(WebPayload, locator="//expiryDay", key="day", optional=True): pass
+class Instrument(WebPayload, locator="//Instrument", key="instruments", contents=instrument_contents): pass
+class BuyInstrument(Instrument, action=Action.BUY, quantity=1): pass
+class SellInstrument(Instrument, action=Action.SELL, quantity=1): pass
 
-class ETradePreviewPayload(WebPayload, locator="//PreviewOrderRequest"):
-    class Orders(ETradeOrderPayload): pass
+BuyPutInstrument = BuyInstrument + PutProduct
+SellPutInstrument = SellInstrument + PutProduct
+BuyCallInstrument = BuyInstrument + CallProduct
+SellCallInstrument = SellInstrument + CallProduct
+BuyStockInstrument = BuyInstrument + StockProduct
+SellStockInstrument = SellInstrument + StockProduct
 
-class ETradePlacePayload(WebPayload, locator="//PlaceOrderRequest"):
-    class Previews(WebPayload, locator="//PreviewIds/previewId", key="previews", collection=True): pass
-    class Orders(ETradeOrderPayload): pass
+stranglelong_payloads = {"put|long": BuyPutInstrument, "call|long": BuyCallInstrument}
+collarlong_payloads = {"put|long": BuyPutInstrument, "call|short": SellCallInstrument, "stock|long": BuyStockInstrument}
+collarshort_payloads = {"put|short": SellPutInstrument, "call|long": BuyCallInstrument, "stock|short": SellStockInstrument}
+verticalput_payloads = {"put|long": BuyPutInstrument, "put|short": SellPutInstrument}
+verticalcall_payloads = {"call|long": BuyCallInstrument, "call|short": SellCallInstrument}
+condor_payloads = {"put|long": BuyPutInstrument, "call|long": BuyCallInstrument, "put|short": SellPutInstrument, "call|long": SellCallInstrument}
+
+class Order(WebPayload, locator="//Order", key="orders", contents=order_contents): pass
+class StrategyOrder(Order, pricing=Pricing.DEBIT, ordertype=OrderType.SPREADS, tenure=Tenure.FILLKILL, session=Session.MARKET, concurrent=Concurrent.TRUE): pass
+class StrangleOrder(StrategyOrder, payloads=stranglelong_payloads): pass
+class CollarLongOrder(StrategyOrder, payloads=collarlong_payloads): pass
+class CollarShortOrder(StrategyOrder, payloads=collarshort_payloads): pass
+class VerticalPutOrder(StrategyOrder, payloads=verticalput_payloads): pass
+class VerticalCallOrder(StrategyOrder, payloads=verticalcall_payloads): pass
+class CondorOrder(StrategyOrder, payloads=condor_payloads): pass
+
+order_payloads = {"strangle|long": StrangleOrder, "collar|long": CollarLongOrder, "collar|short": CollarShortOrder}
+order_payloads.update({"vertical|put": VerticalPutOrder, "vertical|call": VerticalCallOrder, "condor": CondorOrder})
+
+class ETradePreviewPayload(WebPayload, payloads=order_payloads): pass
+class ETradePlacePayload(WebPayload, contents=place_contents, payloads=order_payloads): pass
 
 
 class ETradeOrderData(WebJSON, locator="//Order", key="orders", collection=True):
@@ -85,7 +102,7 @@ class ETradeOrderData(WebJSON, locator="//Order", key="orders", collection=True)
 
     class Price(WebJSON.Json, locator="//priceValue", key="price", parser=np.float32): pass
     class Pricing(WebJSON.Json, locator="//priceType", key="terms", parser=Pricing.__getitem__): pass
-    class Contents(WebJSON.Json, locator="//orderType", key="contents", parser=Contents.__getitem__): pass
+    class Contents(WebJSON.Json, locator="//orderType", key="contents", parser=OrderType.__getitem__): pass
     class Tenure(WebJSON.Json, locator="//orderTerm", key="tenure", parser=Tenure.__getitem__): pass
     class Session(WebJSON.Json, locator="//marketSession", key="session", parser=Session.__getitem__): pass
     class Concurrent(WebJSON.Json, locator="//allOrNone", key="concurrent", parser=Concurrent.__getitem__): pass
@@ -122,27 +139,40 @@ class ETradePlacePage(WebJsonPage): pass
 
 
 order_pages = {"preview": ETradePreviewPage, "place": ETradePlacePage}
-class ETradeOrderUploader(Uploader, pages=order_pages):
+class ETradePreviewUploader(Uploader, pages={"preview": ETradePreviewPage}):
     def execute(self, contents, *args, funds, apy=0, **kwargs):
         ticker, expire, strategy, securities, dataset = contents
         assert isinstance(dataset, xr.Dataset)
-
         dataframe = dataset.to_dask_dataframe() if bool(dataset.chunks) else dataset.to_dataframe()
         dataframe = dataframe.where(dataframe["apy"] >= apy)
         dataframe = dataframe.where(dataframe["cost"] <= funds)
         dataframe = dataframe.dropna(how="all")
+        for preview in self.previews(dataframe, strategy, securities):
+            yield
 
+    def preview(self, dataframe, strategy, securities):
+        for partition in self.partitions(dataframe):
+            for orders in self.orders(partition, strategy, securities):
+                preview = ETradePreviewPayload(orders)
+                yield preview
+
+    @staticmethod
+    def partitions(dataframe):
         if not hasattr(dataframe, "npartitions"):
-            dataframe = dataframe.sort_values("apy", axis=1, ascending=False, ignore_index=True, inplace=False)
-            for row in dataframe.to_dict("records"):
-                order = ETradeOrderPayload(order)
+            yield dataframe
+            return
+        for index in dataframe.npartitions:
+            partition = dataframe.get_partition(index).compute()
+            yield partition
 
-        else:
-            for index in dataframe.npartitions:
-                partition = dataframe.get_partition(index).compute()
-                partition = partition.sort_values("apy", axis=1, ascending=False, ignore_index=True, inplace=False)
-                for row in partition.to_dict("records"):
-                    order = ETradeOrderPayload(order)
+    @staticmethod
+    def orders(partition, strategy, securities):
+        partition = partition.sort_values("apy", axis=1, ascending=False, ignore_index=True, inplace=False)
+        for record in partition.to_dict("records"):
+            product = {"price": record["spot"]} | {attr: getattr(record["expire"], attr) for attr in ("year", "month", "day")}
+            instruments = {str(security): ({"strike": record[str(security)]} if str(security) in record else {}) | product for security in securities}
+            orders = {str(strategy): instruments}
+            return orders
 
 
 
