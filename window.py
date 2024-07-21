684d93b333cc421d0e80759d5236e8767ef36d52
jkc <jackcookchee@yahoo.com>
2024-07-21 18:20:21 -0500

    Updated 1/10/2024

--- window.py
+++ window.py
@@ -9,8 +9,8 @@ Created on Weds Jul 19 2024
 import numpy as np
 import tkinter as tk
 
-from support.windows import Application, Window, Frame, Table, Button, Column, Text
 from finance.variables import Variables
+from support.windows import Stencils
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
@@ -19,47 +19,53 @@ __copyright__ = "Copyright 2024, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-class Product(Frame):
-    strategy = Text("strategy", font="Arial 10 bold", justify=tk.LEFT)
-    contract = Text("contract", font="Arial 10", justify=tk.LEFT)
-    security = Text("security", font="Arial 10", justify=tk.LEFT)
+class Product(Stencils.Frame):
+    strategy = Stencils.Text("strategy", font="Arial 10 bold", justify=tk.LEFT)
+    contract = Stencils.Text("contract", font="Arial 10", justify=tk.LEFT)
+    security = Stencils.Text("security", font="Arial 10", justify=tk.LEFT)
 
 
-class Appraisal(Frame):
-    valuation = Text("valuation", font="Arial 10 bold", justify=tk.LEFT)
-    earnings = Text("earnings", font="Arial 10", justify=tk.LEFT)
-    cashflow = Text("cashflow", font="Arial 10", justify=tk.LEFT)
-    size = Text("size", font="Arial 10", justify=tk.LEFT)
+class Appraisal(Stencils.Frame):
+    valuation = Stencils.Text("valuation", font="Arial 10 bold", justify=tk.LEFT)
+    earnings = Stencils.Text("earnings", font="Arial 10", justify=tk.LEFT)
+    cashflow = Stencils.Text("cashflow", font="Arial 10", justify=tk.LEFT)
+    size = Stencils.Text("size", font="Arial 10", justify=tk.LEFT)
 
 
-class Pursue(Button, title="Pursue"): pass
-class Abandon(Button, title="Abandon"): pass
-class Success(Button, title="Success"): pass
-class Failure(Button, title="Failure"): pass
+class Pursue(Stencils.Button, title="Pursue", action=): pass
+class Abandon(Stencils.Button, title="Abandon", action=): pass
+class Success(Stencils.Button, title="Success", action=): pass
+class Failure(Stencils.Button, title="Failure", action=): pass
 
 
-class HoldingsWindow(Window): pass
+class Prospect(Stencils.Window, title="Prospect", layout=[[Product, Appraisal], [Pursue, Abandon]]): pass
+class Pending(Stencils.Window, title="Pending", layout=[[Product, Appraisal], [Success, Failure]]): pass
+class Accepted(Stencils.Window, title="Accepted", layout=[[Product, Appraisal], []]): pass
+class Rejected(Stencils.Window, title="Rejected", layout=[[Product, Appraisal], []]): pass
+
+
+class HoldingsTable(Stencils.Table, action=):
+    tag = Stencils.Column("tag", width=5, parser=lambda row: f"{row.tag:.0f}")
+    valuation = Stencils.Column("valuation", width=10, parser=lambda row: str(row.valuation))
+    strategy = Stencils.Column("strategy", width=10, parser=lambda row: str(row.strategy))
+    contract = Stencils.Column("contract", width=10, parser=lambda row: f"{str(row.contract.ticker)}\n{str(row.expire.strftime('%Y%m%d'))}")
+    security = Stencils.Column("security", width=20, parser=lambda row: "\n".join([f"{str(key)}={int(value):.02}" for key, value in iter(row) if key in list(Variables.Securities) and not np.isnan(value)]))
+    apy = Stencils.Column("apy", width=10, parser=lambda target: f"{target.profit.apy * 100:.0f}% / YR")
+    tau = Stencils.Column("tau", width=10, parser=lambda target: f"{target.profit.tau:.0f} DYS")
+    npv = Stencils.Column("npv", width=10, parser=lambda target: f"${target.value.profit:,.0f}")
+    cost = Stencils.Column("cost", width=10, parser=lambda target: f"${target.value.cost:,.0f}")
+    size = Stencils.Column("size", width=10, parser=lambda target: f"{target.size:,.0f} CNT")
+
+
+class HoldingsWindow(Stencils.Window, layout=[[HoldingsTable]]): pass
 class AcquisitionWindow(HoldingsWindow, title="Acquisitions"): pass
 class DivestitureWindow(HoldingsWindow, title="Divestitures"): pass
 
 
-class HoldingsTable(Table):
-    tag = Column("tag", width=5, parser=lambda row: f"{row.tag:.0f}")
-    valuation = Column("valuation", width=10, parser=lambda row: str(row.valuation))
-    strategy = Column("strategy", width=10, parser=lambda row: str(row.strategy))
-    contract = Column("contract", width=10, parser=lambda row: f"{str(row.contract.ticker)}\n{str(row.expire.strftime('%Y%m%d'))}")
-    security = Column("security", width=20, parser=lambda row: "\n".join([f"{str(key)}={int(value):.02}" for key, value in iter(row) if key in list(Variables.Securities) and not np.isnan(value)]))
-    apy = Column("apy", width=10, parser=lambda target: f"{target.profit.apy * 100:.0f}% / YR")
-    tau = Column("tau", width=10, parser=lambda target: f"{target.profit.tau:.0f} DYS")
-    npv = Column("npv", width=10, parser=lambda target: f"${target.value.profit:,.0f}")
-    cost = Column("cost", width=10, parser=lambda target: f"${target.value.cost:,.0f}")
-    size = Column("size", width=10, parser=lambda target: f"{target.size:,.0f} CNT")
-
-
-class PaperTradeWindow(Window, title="PaperTrading"):
+class PaperTradeWindow(Stencils.Window, title="PaperTrading"):
     pass
 
 
-class PaperTradeApplication(Application, window=PaperTradeWindow):
+class PaperTradeApplication(Stencils.Application, window=PaperTradeWindow):
     pass
 
