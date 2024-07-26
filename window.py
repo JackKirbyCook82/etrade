ecc3520eaca25c53fc615c16f749b77ea3656b16
JackKirbyCook82 <jackcookchee@yahoo.com>
2024-07-26 17:17:10 -0500

    Updated 1/10/2024

--- window.py
+++ window.py
@@ -10,7 +10,7 @@ import numpy as np
 import tkinter as tk
 
 from finance.variables import Variables, Contract
-from support.windows import Application, Stencils, Content, Column
+from support.windows import Application, Stencils, Layouts
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
@@ -26,34 +26,34 @@ security_locator = lambda row: {(str(security), ""): row[str(security)] for secu
 
 
 class Product(Stencils.Frame):
-    strategy = Content(Stencils.Label, text="strategy", font="Arial 10 bold", justify=tk.LEFT, locator=(0, 0))
-    contract = Content(Stencils.Label, text="contract", font="Arial 10", justify=tk.LEFT, locator=(1, 0))
-    security = Content(Stencils.Label, text="security", font="Arial 10", justify=tk.LEFT, locator=(2, 0))
+    strategy = Layouts.Widget(Stencils.Label, text="strategy", font="Arial 10 bold", justify=tk.LEFT, locator=(0, 0))
+    contract = Layouts.Widget(Stencils.Label, text="contract", font="Arial 10", justify=tk.LEFT, locator=(1, 0))
+    security = Layouts.Widget(Stencils.Label, text="security", font="Arial 10", justify=tk.LEFT, locator=(2, 0))
 
 class Appraisal(Stencils.Frame):
-    valuation = Content(Stencils.Label, text="valuation", font="Arial 10 bold", justify=tk.LEFT, locator=(0, 0))
-    earnings = Content(Stencils.Label, text="earnings", font="Arial 10", justify=tk.LEFT, locator=(1, 0))
-    cashflow = Content(Stencils.Label, text="cashflow", font="Arial 10", justify=tk.LEFT, locator=(2, 0))
-    size = Content(Stencils.Label, text="size", font="Arial 10", justify=tk.LEFT, locator=(3, 0))
+    valuation = Layouts.Widget(Stencils.Label, text="valuation", font="Arial 10 bold", justify=tk.LEFT, locator=(0, 0))
+    earnings = Layouts.Widget(Stencils.Label, text="earnings", font="Arial 10", justify=tk.LEFT, locator=(1, 0))
+    cashflow = Layouts.Widget(Stencils.Label, text="cashflow", font="Arial 10", justify=tk.LEFT, locator=(2, 0))
+    size = Layouts.Widget(Stencils.Label, text="size", font="Arial 10", justify=tk.LEFT, locator=(3, 0))
 
 
 class HoldingsScroll(Stencils.Scroll): pass
 class HoldingsTable(Stencils.Table):
-    tag = Column(text="tag", width=5, parser=lambda tag: f"{tag:.0f}", locator=lambda row: row[("tag", "")])
-    valuation = Column(text="valuation", width=10, parser=lambda valuation: str(valuation), locator=lambda row: row[("valuation", "")])
-    strategy = Column(text="strategy", width=10, parser=lambda strategy: str(strategy), locator=lambda row: row[("strategy", "")])
-    contract = Column(text="contract", width=10, parser=contract_parser, locator=contract_locator)
-    security = Column(text="security", width=20, parser=security_parser, locator=security_locator)
-    apy = Column(text="apy", width=10, parser=lambda apy: f"{apy * 100:.0f}% / YR", locator=lambda row: row[("apy", "minimum")])
-    tau = Column(text="tau", width=10, parser=lambda tau: f"{tau:.0f} DY", locator=lambda row: row[("tau", "")])
-    npv = Column(text="npv", width=10, parser=lambda npv: f"${npv:,.0f}", locator=lambda row: row[("npv", "minimum")])
-    cost = Column(text="cost", width=10, parser=lambda cost: f"${cost:,.0f}", locator=lambda row: row[("cost", "minimum")])
-    size = Column(text="size", width=10, parser=lambda size: f"{size:,.0f} CT", locator=lambda row: row[("size", "")])
+    tag = Layouts.Column(text="tag", width=50, parser=lambda tag: f"{tag:.0f}", locator=lambda row: row[("tag", "")])
+    valuation = Layouts.Column(text="valuation", width=200, parser=lambda valuation: str(valuation), locator=lambda row: row[("valuation", "")])
+    strategy = Layouts.Column(text="strategy", width=200, parser=lambda strategy: str(strategy), locator=lambda row: row[("strategy", "")])
+    contract = Layouts.Column(text="contract", width=200, parser=contract_parser, locator=contract_locator)
+    security = Layouts.Column(text="security", width=200, parser=security_parser, locator=security_locator)
+    apy = Layouts.Column(text="apy", width=100, parser=lambda apy: f"{apy * 100:.0f}% / YR", locator=lambda row: row[("apy", "minimum")])
+    tau = Layouts.Column(text="tau", width=100, parser=lambda tau: f"{tau:.0f} DY", locator=lambda row: row[("tau", "")])
+    npv = Layouts.Column(text="npv", width=100, parser=lambda npv: f"${npv:,.0f}", locator=lambda row: row[("npv", "minimum")])
+    cost = Layouts.Column(text="cost", width=100, parser=lambda cost: f"${cost:,.0f}", locator=lambda row: row[("cost", "minimum")])
+    size = Layouts.Column(text="size", width=100, parser=lambda size: f"{size:,.0f} CT", locator=lambda row: row[("size", "")])
 
 
 class Holdings(Stencils.Frame):
-    table = Content(HoldingsTable, locator=(0, 0))
-    vertical = Content(HoldingsScroll, orientation=tk.VERTICAL, locator=(0, 1))
+    table = Layouts.Widget(HoldingsTable, locator=(0, 0))
+    vertical = Layouts.Widget(HoldingsScroll, orientation=tk.VERTICAL, locator=(0, 1))
 
 class Acquisitions(Holdings): pass
 class Divestitures(Holdings): pass
@@ -66,26 +66,26 @@ class Failure(Stencils.Button): pass
 
 
 class StatusWindow(Stencils.Window):
-    product = Content(Product, locator=(0, 0))
-    appraisal = Content(Appraisal, locator=(0, 1))
+    product = Layouts.Widget(Product, locator=(0, 0))
+    appraisal = Layouts.Widget(Appraisal, locator=(0, 1))
 
 class AcceptedWindow(StatusWindow): pass
 class RejectedWindow(StatusWindow): pass
 
 class ProspectWindow(StatusWindow):
-    pursue = Content(Pursue, text="pursue", font="Arial 10", justify=tk.CENTER, locator=(1, 0))
-    abandon = Content(Abandon, text="abandon", font="Arial 10", justify=tk.CENTER, locator=(1, 1))
+    pursue = Layouts.Widget(Pursue, text="pursue", font="Arial 10", justify=tk.CENTER, locator=(1, 0))
+    abandon = Layouts.Widget(Abandon, text="abandon", font="Arial 10", justify=tk.CENTER, locator=(1, 1))
 
 class PendingWindow(StatusWindow):
-    success = Content(Success, text="success", font="Arial 10", justify=tk.CENTER, locator=(1, 0))
-    failure = Content(Failure, text="failure", font="Arial 10", justify=tk.CENTER, locator=(1, 1))
+    success = Layouts.Widget(Success, text="success", font="Arial 10", justify=tk.CENTER, locator=(1, 0))
+    failure = Layouts.Widget(Failure, text="failure", font="Arial 10", justify=tk.CENTER, locator=(1, 1))
 
 class PaperTradingWindow(Stencils.Notebook):
-    acquisitions = Content(Acquisitions, sticky=tk.NSEW, locator=(0, 0))
-    divestitures = Content(Divestitures, sticky=tk.NSEW, locator=(0, 0))
+    acquisitions = Layouts.Widget(Acquisitions, locator=(0, 0))
+    divestitures = Layouts.Widget(Divestitures, locator=(0, 0))
 
 
-class PaperTradeApplication(Application):
+class PaperTradeApplication(Application, window=PaperTradingWindow, title="PaperTrading"):
     def __init__(self, *args, acquisitions, divestitures, **kwargs):
         super().__init__(*args, **kwargs)
         self.__acquisitions = acquisitions
