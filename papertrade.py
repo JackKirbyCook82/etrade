a0be00e3ae2c97e433e93992ca063c36915f80c7
JackKirbyCook82 <jackcookchee@yahoo.com>
2024-11-19 17:00:37 -0600

    Updated 1/10/2024

---
+++ papertrade.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tues Nov 19 2024
+@name:   ETrade PaperTrade Objects
+@author: Jack Kirby Cook
+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = []
+__copyright__ = "Copyright 2024, Jack Kirby Cook"
+__license__ = "MIT License"
+
+
+
