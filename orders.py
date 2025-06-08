0c361fd68f63fbc74840cc3f1a5b6367b1e6cb07
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-06-07 20:39:06 -0500

    Updated 1/10/2024

--- orders.py
+++ orders.py
@@ -6,11 +6,12 @@
 
 """
 
+import numpy as np
 import pandas as pd
 from abc import ABC
 from collections import namedtuple as ntuple
 
-from finance.variables import Variables
+from finance.variables import Querys, Variables, Securities
 from webscraping.weburl import WebURL, WebPayload
 from webscraping.webpages import WebJSONPage
 from support.mixins import Emptying, Logging, Naming
@@ -21,10 +22,35 @@ __copyright__ = "Copyright 2023, Jack Kirby Cook"
 __license__ = "MIT License"
 
 
-class ETradeValuation(Naming): pass
-class ETradeSecurity(Naming, ABC): pass
-class ETradeStock(ETradeSecurity): pass
-class ETradeOption(ETradeSecurity): pass
+tenure_formatter = lambda order: {Variables.Markets.Tenure.DAY: "GOOD_FOR_DAY", Variables.Markets.Tenure.FILLKILL: "FILL_OR_KILL"}[order.tenure]
+action_formatter = lambda instrument: {Variables.Markets.Action.BUY: "BUY", Variables.Markets.Action.SELL: "SELL"}[instrument.action]
+
+
+class ETradeProduct(Naming, ABC, fields=["ticker", "instrument", "option"]):
+    def __new__(cls, security, *args, **kwargs):
+        security = dict(instrument=security.instrument, option=security.option, position=security.position)
+        return super().__new__(cls, *args, **security, **kwargs)
+
+class ETradeOption(ETradeProduct, fields=["expire", "strike"]): pass
+class ETradeStock(ETradeProduct): pass
+
+class ETradeInstrument(Naming, fields=["position", "quantity", "product"]):
+    def __new__(cls, security, *args, quantity, **kwargs):
+        if security.instrument == Variables.Securities.Instrument.OPTION:
+            parameters = dict(instrument=security.instrument, option=security.option, quantity=quantity * 1)
+            product = ETradeOption(*args, **parameters, **kwargs)
+        elif security.instrument == Variables.Securities.Instrument.STOCK:
+            parameters = dict(instrument=security.instrument, quantity=quantity * 100)
+            product = ETradeStock(*args, **parameters, **kwargs)
+        else: raise ValueError(security.instrument)
+        parameters = dict(position=security.position, product=product)
+        return super().__new__(cls, *args, **parameters, **kwargs)
+
+class ETradeValuation(Naming, fields=["npv"]):
+    def __str__(self): return f"${self.npv.min():.0f} -> ${self.npv.max():.0f}"
+
+class ETradeOrder(Naming, ABC, fields=["term", "tenure", "limit", "stop", "instruments"]):
+    pass
 
 
 class ETradeOrderURL(WebURL, domain="https://api.etrade.com", path=["v1", "accounts"]): pass
@@ -38,28 +64,28 @@ class ETradePlaceURL(ETradeOrderURL):
 
 
 class ETradeOrderPayload(WebPayload, key="order", locator="Order", fields={"allOrNone": "true", "marketSession": "REGULAR", "pricing": "NET_DEBIT"}, multiple=False, optional=False):
-    limit = lambda order: {"limitPrice": None}
-    stop = lambda order: {"stopPrice": None}
-    tenure = lambda order: {"orderTerm": None}
+    limit = lambda order: {"limitPrice": f"{order.limit:.02f}"} if order.term in (Variables.Markets.Term.LIMIT, Variables.Markets.Term.STOPLIMIT) else {}
+    stop = lambda order: {"stopPrice": f"{order.stop:.02f}"} if order.term in (Variables.Markets.Term.STOP, Variables.Markets.Term.STOPLIMIT) else {}
+    tenure = lambda order: {"orderTerm": tenure_formatter(order)}
 
     class Instrument(WebPayload, key="instrument", locator="Instrument", multiple=True, optional=False):
-        action = lambda security: {"orderAction": None}
-        quantity = lambda security: {"quantity": None}
+        action = lambda instrument: {"orderAction": action_formatter(instrument)}
+        quantity = lambda instrument: {"quantity": str(instrument.quantity)}
 
         class Product(WebPayload, key="product", locator="Product", multiple=False, optional=False):
-            ticker = lambda security: {"symbol": None}
-            expire = lambda security: {"expiryYear": None, "expiryMonth": None, "expiryDay": None}
-            instrument = lambda security: {"securityType": None}
-            option = lambda security: {"callPut": None}
-            strike = lambda security: {"strikePrice": None}
+            expire = lambda product: {"expiryYear": str(product.expire.year), "expiryMonth": str(product.expire.month), "expiryDay": str(product.expire.day)} if isinstance(product, ETradeOption) else {}
+            instrument = lambda product: {"securityType": {Variables.Securities.Instrument.OPTION: "OPTN", Variables.Securities.Instrument.STOCK: "EQ"}[product.instrument]}
+            option = lambda product: {"callPut": str(product.option).upper()} if isinstance(product, ETradeOption) else {}
+            strike = lambda product: {"strikePrice": product.strike} if isinstance(product, ETradeOption) else {}
+            ticker = lambda product: {"symbol": str(product.ticker)}
 
 
 class ETradePreviewPayload(WebPayload, key="preview", locator="PreviewOrderRequest", dependents=[ETradeOrderPayload], fields={"orderType": "SPREADS"}, multiple=False, optional=False):
     identity = lambda order: {"clientOrderId": None}
 
 class ETradePlacePayload(WebPayload, key="place", locator="PlaceOrderRequest", dependents=[ETradeOrderPayload], fields={"orderType": "SPREADS"}, multiple=False, optional=False):
-    identity = lambda order: {"clientOrderId": None}
     preview = lambda order: {"PreviewIds": {"previewId": None}}
+    identity = lambda order: {"clientOrderId": None}
 
 
 class ETradeOrderPage(WebJSONPage):
@@ -92,6 +118,10 @@ class ETradeOrderUploader(Emptying, Logging, title="Uploaded"):
     def execute(self, prospects, *args, **kwargs):
         assert isinstance(prospects, pd.DataFrame)
         if self.empty(prospects): return
+
+        print(prospects)
+        raise Exception()
+
         for order, valuation in self.calculator(prospects, *args, **kwargs):
             self.upload(order, *args, **kwargs)
             securities = ", ".join(list(map(str, order.options + order.stocks)))
@@ -104,7 +134,19 @@ class ETradeOrderUploader(Emptying, Logging, title="Uploaded"):
     def calculator(prospects, *args, term, tenure, **kwargs):
         assert term in (Variables.Markets.Term.MARKET, Variables.Markets.Term.LIMIT)
         for index, prospect in prospects.iterrows():
-            pass
+            strategy, quantity = prospect[["strategy", "quantity"]].droplevel(1).values
+            settlement = prospect[list(Querys.Settlement)].droplevel(1).to_dict()
+            options = prospect[list(map(str, Securities.Options))].droplevel(1).to_dict()
+            options = {Securities.Options[option]: strike for option, strike in options.items() if not np.isnan(strike)}
+            stocks = {Securities.Stocks[stock] for stock in strategy.stocks}
+            breakeven = prospect[("spot", Variables.Scenario.BREAKEVEN)]
+            current = prospect[("spot", Variables.Scenario.CURRENT)]
+            assert current >= breakeven and quantity >= 1
+            options = [ETradeInstrument(security, strike=strike, **settlement) for security, strike in options.items()]
+            stocks = [ETradeInstrument(security, **settlement) for security, strike in stocks]
+            valuation = ETradeValuation(npv=prospect.xs("npv", axis=0, level=0, drop_level=True))
+            order = ETradeOrder(instruments=options + stocks, term=term, tenure=tenure, limit=-breakeven, stop=None, quantity=1)
+            yield order, valuation
 
     @property
     def pages(self): return self.__pages
--- service.py
+++ service.py
@@ -8,11 +8,12 @@ Created on Thurs May 29 2025
 
 from webscraping.webreaders import WebService
 from webscraping.webdrivers import WebDriver
+from webscraping.webpages import WebELMTPage
 from webscraping.webdatas import WebELMT
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["ETradeService"]
+__all__ = ["ETradeServiceReader"]
 __copyright__ = "Copyright 2025, Jack Kirby Cook"
 __license__ = "MIT License"
 
@@ -30,27 +31,35 @@ class ETradeAcceptData(WebELMT.Button, locator=r"//input[@id='acceptSubmit']", k
 class ETradeSecurity(WebELMT.Value, locator=r"//input[@type='text']", key="security"): pass
 
 
-class ETradeService(WebService, authorize=AUTHORIZE, request=REQUEST, access=ACCESS, base=BASE):
-    def __init__(self, *args, executable, authorize, timeout=60, port=None, **kwargs):
-        super().__init__(*args, **kwargs)
+class ETradeServicePage(WebELMTPage):
+    def execute(self, url, *args, authorize, timeout, **kwargs):
+        self.load(*args, url=str(url), **kwargs)
+        username = ETradeUsernameData(self.elmt, *args, timeout=timeout, **kwargs)
+        password = ETradePasswordData(self.elmt, *args, timeout=timeout, **kwargs)
+        login = ETradeLoginData(self.elmt, *args, timeout=timeout, **kwargs)
+        username.fill(authorize.username)
+        password.fill(authorize.password)
+        with self.delayer: login.click()
+        accept = ETradeAcceptData(self.elmt, *args, timeout=timeout, **kwargs)
+        with self.delayer: accept.click()
+        security = ETradeSecurity(self.elmt, *args, timeout=timeout, **kwargs)
+        return security(*args, **kwargs)
+
+
+class ETradeServiceReader(WebService, authorize=AUTHORIZE, request=REQUEST, access=ACCESS, base=BASE):
+    def __init__(self, *args, executable, authorize, delay, timeout=60, port=None, **kwargs):
+        super().__init__(*args, delay=delay, **kwargs)
         self.__executable = executable
         self.__timeout = int(timeout)
         self.__authorize = authorize
+        self.__delay = delay
         self.__port = port
 
     def security(self, url, *args, **kwargs):
-        with WebDriver(executable=self.executable, delay=self.delay, timeout=self.timeout, port=self.port) as source:
-            source.load(*args, url=str(url), **kwargs)
-            username = ETradeUsernameData(source.elmt, *args, timeout=self.timeout, **kwargs)
-            password = ETradePasswordData(source.elmt, *args, timeout=self.timeout, **kwargs)
-            login = ETradeLoginData(source.elmt, *args, timeout=self.timeout, **kwargs)
-            username.fill(self.authorize.username)
-            password.fill(self.authorize.password)
-            login.click()
-            accept = ETradeAcceptData(source.elmt, *args, timeout=self.timeout, **kwargs)
-            accept.click()
-            security = ETradeSecurity(source.elmt, *args, timeout=self.timeout, **kwargs)
-            return security(*args, **kwargs)
+        with WebDriver(executable=self.executable, timeout=self.timeout, delay=self.delay, port=self.port) as source:
+            page = ETradeServicePage(*args, source=source, **kwargs)
+            security = page(url, *args, timeout=self.timeout, authorize=self.authorize, **kwargs)
+            return security
 
     @property
     def executable(self): return self.__executable
@@ -58,6 +67,8 @@ class ETradeService(WebService, authorize=AUTHORIZE, request=REQUEST, access=ACC
     def authorize(self): return self.__authorize
     @property
     def timeout(self): return self.__timeout
+    @property
+    def delay(self): return self.__delay
     @property
     def port(self): return self.__port
 
